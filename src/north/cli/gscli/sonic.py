3a508a4a51b21101ef02f8df63f6d95a2f88e0ad
Shivashankar Chitragar <pharshad@palcnetworks.com>
2020-10-21 08:45:03 -0400

    Addressed issues #119 ,#71, #70 with corresponding changes in sonic.py

--- src/north/cli/gscli/sonic.py
+++ src/north/cli/gscli/sonic.py
@@ -19,38 +19,40 @@ class Vlan(object):
     def __init__(self, conn, parent):
         self.session = conn.start_session()
         self.sr_op = sysrepo_wrap()
-        self.tree = self.sr_op.get_data_ly("{}".format(self.XPATH), 'operational')
-        self.treeport = self.sr_op.get_data_ly("{}".format(self.XPATHport), 'operational')
+        self.tree = self.sr_op.get_data_ly ("{}".format(self.XPATH), 'operational')
+        self.treeport = self.sr_op.get_data_ly ("{}".format(self.XPATHport), 'operational')
         try:
-            self._vlan_map = json.loads(self.tree.print_mem("json"))['sonic-vlan:sonic-vlan']['VLAN']['VLAN_LIST']
+            self._vlan_map = json.loads (self.tree.print_mem("json"))['sonic-vlan:sonic-vlan']['VLAN']['VLAN_LIST']
         except KeyError as error:
             pass
 
     def show_vlan(self, details = 'details'):
-        #self.session.switch_datastore('operational')
-        dl1 = self.tree.print_mem("json")
-        dl2 = self.treeport.print_mem("json")
-        dl1 = json.loads(dl1)
-        dl2 = json.loads(dl2)
+        dl1 = self.tree.print_mem ("json")
+        dl2 = self.treeport.print_mem ("json")
+        dl1 = json.loads (dl1)
+        dl2 = json.loads (dl2)
         
         if dl1 == {}:
-            print(tabulate([],["VLAN ID","Port","Port Tagging"], tablefmt = "pretty"))
+            print (tabulate ([], ["VLAN ID", "Port", "Port Tagging"], tablefmt = "pretty"))
         else:
-            dl1 = dl1["sonic-vlan:sonic-vlan"]["VLAN"]["VLAN_LIST"]
-            dl2 = dl2["sonic-vlan:sonic-vlan"]["VLAN_MEMBER"]["VLAN_MEMBER_LIST"]
+            try:
+                dl1 = dl1["sonic-vlan:sonic-vlan"]["VLAN"]["VLAN_LIST"]
+                dl2 = dl2["sonic-vlan:sonic-vlan"]["VLAN_MEMBER"]["VLAN_MEMBER_LIST"]
+            except KeyError as error:
+                pass
             dln = []
             for i in range(len(dl1)):
                 if "members" in dl1[i]:
                     for j in range(len(dl1[i]["members"])):
                         tg=''
                         for k in range(len(dl2)):
-                            if dl2[k]["name"]==dl1[i]["name"] and dl2[k]["ifname"]==dl1[i]["members"][j]:
-                                tg=dl2[k]["tagging_mode"]
+                            if dl2[k]["name"] == dl1[i]["name"] and dl2[k]["ifname"] == dl1[i]["members"][j]:
+                                tg = dl2[k]["tagging_mode"]
                                 break
-                        dln.append([dl1[i]["vlanid"], dl1[i]["members"][j], tg])
+                        dln.append ([dl1[i]["vlanid"], dl1[i]["members"][j], tg])
                 else:
-                    dln.append([dl1[i]["vlanid"],"N/A","N/A"])
-            print(tabulate(dln,["VLAN ID","Port","Port Tagging"],  tablefmt = "pretty"))
+                    dln.append ([dl1[i]["vlanid"], "-", "-"])
+            print (tabulate (dln, ["VLAN ID", "Port", "Port Tagging"],  tablefmt = "pretty"))
         self.session.switch_datastore('running')
 
     def _vlan_components(self):
@@ -67,34 +69,41 @@ class Port(object):
         return "{}[ifname='{}']".format(self.path, ifname)
 
     def __init__(self, conn, parent):
-        self.session = conn.start_session()
-        self.sr_op = sysrepo_wrap()
+        self.session = conn.start_session ()
+        self.sr_op = sysrepo_wrap ()
         try:
-            self.tree = self.sr_op.get_data(self.XPATH, 'operational')
-            self._ifname_map = list(self.tree['sonic-port']['PORT']['PORT_LIST'])
+            self.tree = self.sr_op.get_data (self.XPATH, 'operational')
+            self._ifname_map = list (self.tree['sonic-port']['PORT']['PORT_LIST'])
         except KeyError as error:
-            print("Port list is not configured")
+            print ("Port list is not configured")
         except sr.errors.SysrepoNotFoundError as error:
-            print("sonic-mgmt is down")
+            print ("sonic-mgmt is down")
 
     
     def show_interface(self, details = 'description'):
         #TODO:switch to operational when sonic_south is fixed
         try:
-            self.tree = self.sr_op.get_data(self.XPATH)
-            self._ifname_map = list(self.tree['sonic-port']['PORT']['PORT_LIST'])
+            self.tree = self.sr_op.get_data (self.XPATH)
+            self._ifname_map = list (self.tree['sonic-port']['PORT']['PORT_LIST'])
         except KeyError as error:
-            print("Port list is not configured")
+            print ("Port list is not configured")
         except sr.errors.SysrepoNotFoundError as error:
-            print("sonic-mgmt is down")
+            print ("sonic-mgmt is down")
+        rows = []
         if (details == 'brief'):
-            print ('TODO: brief , Please use "show interface description"')
-        else:
-            headers = ["ifname", "admin_status", "alias"]
-            rows = []
+            headers = ["ifname", "oper_status", "admin_status", "alias"]
             for data in self._ifname_map :
-                rows.append([data["ifname"], data["admin_status"] if "admin_status" in data.keys() else "N/A", data["alias"]])
-            print(tabulate(rows, headers, tablefmt = "pretty"))
+                rows.append ([data["ifname"], data["oper_status"] if "oper_status" in data.keys() else "-",
+                              data["admin_status"] if "admin_status" in data.keys() else "-", data["alias"]])
+            print (tabulate (rows, headers, tablefmt = "pretty"))
+        elif (details == 'description'):
+            headers = ["ifname", "oper_status", "admin_status", "alias", "speed", "mtu"]
+            for data in self._ifname_map :
+                rows.append ([data["ifname"], data["oper_status"] if "oper_status" in data.keys() else "-",
+                              data["admin_status"] if "admin_status" in data.keys() else "-", data["alias"], 
+                              data["speed"] if "speed" in data.keys() else "-",
+                              data["mtu"] if "mtu" in data.keys() else "-"])
+            print (tabulate (rows, headers, tablefmt = "pretty"))
 
 
     def run_conf(self):
@@ -137,32 +146,32 @@ class Port(object):
 
     def set_admin_status(self, ifname, value):
         xpath = self.xpath (ifname) 
-        self.sr_op.set_data('{}/{}'.format(xpath, 'admin_status'), value)
+        self.sr_op.set_data ('{}/{}'.format(xpath, 'admin_status'), value)
     
     def set_mtu(self, ifname, value):
         xpath = self.xpath (ifname) 
-        self.sr_op.set_data('{}/{}'.format(xpath, 'mtu'), value)
+        self.sr_op.set_data ('{}/{}'.format(xpath, 'mtu'), value)
     
     def set_speed(self, ifname, value):
         xpath = self.xpath (ifname) 
-        self.sr_op.set_data('{}/{}'.format(xpath, 'speed'), value)
+        self.sr_op.set_data ('{}/{}'.format(xpath, 'speed'), value)
 
     def show(self, ifname):
         xpath = self.xpath(ifname)
         tree = self.sr_op.get_data(xpath)
         data = [v for v in list((tree)['sonic-port']['PORT']['PORT_LIST'])][0]
-        print_tabular(data, '')
+        print_tabular (data, '')
  
 
 class Sonic(object):
 
     def __init__(self, conn):
-        self.session = conn.start_session()
-        self.port = Port(conn, self)
-        self.vlan = Vlan(conn, self)
+        self.session = conn.start_session ()
+        self.port = Port (conn, self)
+        self.vlan = Vlan (conn, self)
 
     def port_run_conf(self):
-        self.port.run_conf()
+        self.port.run_conf ()
 
 
     def vlan_run_conf(self):
@@ -170,10 +179,10 @@ class Sonic(object):
         pass
 
     def run_conf(self):
-        self.port_run_conf()
+        self.port_run_conf ()
     
     def tech_support(self):
-        print('\nshow vlan brief:\n')
-        self.vlan.show()
+        print('\nshow vlan details:\n')
+        self.vlan.show_vlan ()
         print('\nshow interface description:\n')
-        self.port.show()
+        self.port.show_interface ()
