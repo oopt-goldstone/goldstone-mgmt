f0e4b5864afaea2aa4d93bd4a4f4926c5263f71a
Wataru Ishida <wataru.ishid@gmail.com>
2020-10-22 18:06:43 +0000

    ci: load container images on the hardware and check it's running
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ ci/docker/gs-mgmt-test.Dockerfile
@@ -0,0 +1,3 @@
+FROM gs-builder
+
+RUN pip3 install paramiko scp
---
+++ ci/tools/load.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import paramiko
+import argparse
+import subprocess
+import sys
+from scp import SCPClient
+import time
+
+def ssh(cli, cmd):
+    print(f'ssh: "{cmd}"')
+    _, stdout, stderr = cli.exec_command(cmd)
+    output = []
+    for line in stdout:
+        output.append(line)
+        print(f'stdout: {line}', end='')
+    for line in stderr:
+        print(f'stderr: {line}', end='')
+    ret = stdout.channel.recv_exit_status()
+    if ret != 0:
+        raise Exception(f'{cmd} failed: ret: {ret}')
+
+    return ''.join(output)
+
+def run(cmd):
+    print(f'run: "{cmd}"')
+    subprocess.run(cmd, shell=True, stdout=sys.stdout, stderr=sys.stderr, check=True)
+
+def main(host, username, password):
+    with paramiko.SSHClient() as cli:
+        cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        cli.connect(host, username=username, password=password)
+
+        ssh(cli, 'kubectl delete -f /var/lib/rancher/k3s/server/manifests/mgmt || true') # can fail
+        ssh(cli, 'rm -rf /var/lib/rancher/k3s/server/manifests/mgmt')
+
+        run('docker save -o /tmp/gs-mgmt.tar gs-test/gs-mgmt-debug:latest')
+
+        scp = SCPClient(cli.get_transport())
+        scp.put('/tmp/gs-mgmt.tar', '/tmp')
+        ssh(cli, 'ctr images import /tmp/gs-mgmt.tar')
+
+        scp.put('./ci/k8s', recursive=True, remote_path='/var/lib/rancher/k3s/server/manifests/mgmt')
+
+        ssh(cli, 'rm -rf /dev/shm/sr_*')
+        ssh(cli, 'rm -rf /var/lib/sysrepo/*')
+        ssh(cli, 'kubectl create -f /var/lib/rancher/k3s/server/manifests/mgmt')
+
+        def check_pod(name):
+            max_iteration = 4
+            for i in range(max_iteration):
+                time.sleep(5)
+                status = ssh(cli, f"kubectl get pod -l app={name} -o jsonpath='{{.items[0].status.phase}}'")
+                if status == 'Running':
+                    return
+                print(f'{name} not running yet. status = {status}. waiting.. {i}/{max_iteration}')
+            else:
+                print('timeout')
+                ssh(cli, "kubectl get pods -A")
+                ssh(cli, f"kubectl describe pods -l app={name}")
+                sys.exit(1)
+
+        check_pod('gs-mgmt-onlp')
+        check_pod('gs-mgmt-sonic')
+        check_pod('gs-mgmt-tai')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Goldstone CI tool')
+    parser.add_argument('host')
+    parser.add_argument('--username', default='root')
+    parser.add_argument('--password', default='x1')
+
+    args = parser.parse_args()
+    main(args.host, args.username, args.password)
+
