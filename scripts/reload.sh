8fddfbb0e7b7c6bcdf762f0d865b3ae19ebb1372
Wataru Ishida <wataru.ishid@gmail.com>
2020-10-31 00:01:47 +0000

    build: use reload.sh to install YANG models
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ scripts/reload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -eux
+
+rm -rf /var/lib/sysrepo/* /dev/shm/sr_*
+
+sysrepoctl -s/var/lib/goldstone/yang/gs --install /var/lib/goldstone/yang/gs/goldstone-tai.yang,/var/lib/goldstone/yang/gs/goldstone-onlp.yang
+sysrepoctl -s/var/lib/goldstone/yang/gs --install /var/lib/goldstone/yang/gs/goldstone-interface.yang,/var/lib/goldstone/yang/gs/goldstone-mgmt-interface.yang,/var/lib/goldstone/yang/gs/goldstone-ip.yang,/var/lib/goldstone/yang/gs/goldstone-arp.yang,/var/lib/goldstone/yang/gs/goldstone-portchannel.yang,/var/lib/goldstone/yang/gs/goldstone-vlan.yang,/var/lib/goldstone/yang/gs/goldstone-aaa.yang
