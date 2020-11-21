92e5e7048803e286738e11d7747234b91468b572
Wataru Ishida <wataru.ishid@gmail.com>
2020-11-20 18:45:40 -0800

    yang: update file name to match with module name (#201)
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -5,4 +5,4 @@ set -eux
 rm -rf /var/lib/sysrepo/* /dev/shm/sr_*
 
 sysrepoctl -s/var/lib/goldstone/yang/gs --install /var/lib/goldstone/yang/gs/goldstone-tai.yang,/var/lib/goldstone/yang/gs/goldstone-onlp.yang
-sysrepoctl -s/var/lib/goldstone/yang/gs --install /var/lib/goldstone/yang/gs/goldstone-interface.yang,/var/lib/goldstone/yang/gs/goldstone-mgmt-interface.yang,/var/lib/goldstone/yang/gs/goldstone-ip.yang,/var/lib/goldstone/yang/gs/goldstone-arp.yang,/var/lib/goldstone/yang/gs/goldstone-portchannel.yang,/var/lib/goldstone/yang/gs/goldstone-vlan.yang,/var/lib/goldstone/yang/gs/goldstone-aaa.yang
+sysrepoctl -s/var/lib/goldstone/yang/gs --install /var/lib/goldstone/yang/gs/goldstone-interfaces.yang,/var/lib/goldstone/yang/gs/goldstone-mgmt-interfaces.yang,/var/lib/goldstone/yang/gs/goldstone-ip.yang,/var/lib/goldstone/yang/gs/goldstone-arp.yang,/var/lib/goldstone/yang/gs/goldstone-portchannel.yang,/var/lib/goldstone/yang/gs/goldstone-vlan.yang,/var/lib/goldstone/yang/gs/goldstone-aaa.yang
