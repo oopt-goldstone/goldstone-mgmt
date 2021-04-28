bfe717b3d319ed456261830a3a8d7e3479f1e23e
Wataru Ishida <wataru.ishid@gmail.com>
2021-04-28 18:13:38 +0900

    ci: enhance np2 testing script (#7)
    closes #3
    
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- ci/tools/test_np2.py
+++ ci/tools/test_np2.py
@@ -9,6 +9,50 @@ import tempfile
 
 from .common import *
 
+def run_np2_cli(cli, host, commands):
+    run("rm -rf id_rsa id_rsa.pub")
+    run("ssh-keygen -f id_rsa -N ''")
+    ssh(cli, "mkdir -p /home/admin/.ssh")
+    scp = SCPClient(cli.get_transport())
+    scp.put("id_rsa.pub", "/home/admin/.ssh/authorized_keys")
+    ssh(cli, "chown admin:admin /home/admin/.ssh/authorized_keys")
+
+    cmd = "\n".join(commands)
+
+    with open("/tmp/check_np2.sh", "w") as f:
+        f.write(
+            f"""#!/bin/sh
+netopeer2-cli <<EOF
+auth pref publickey 4
+auth keys add id_rsa.pub id_rsa
+auth hostkey-check disable
+connect --host {host} --login admin
+{cmd}
+EOF
+"""
+        )
+    run("chmod +x /tmp/check_np2.sh && /tmp/check_np2.sh")
+
+def test_get(cli, host):
+    run_np2_cli(cli, host, ["status", "get --filter-xpath '/goldstone-interfaces:*'", "get-config --source running --filter-xpath '/goldstone-tai:modules'"])
+
+
+def test_interface_admin_set(cli, host):
+    ssh(cli, "gscli -c 'clear datastore all'")
+
+    config = "config.xml"
+    with open(config, "w") as f:
+        f.write("""<interfaces xmlns="http://goldstone.net/yang/goldstone-interfaces">
+  <interface>
+    <name>Ethernet1_1</name>
+    <admin-status>down</admin-status>
+  </interface>
+</interfaces>""")
+
+    run_np2_cli(cli, host, [f"edit-config --target running --config={config}", "commit"])
+    output = ssh(cli, "gscli -c 'show running-config'")
+    assert "shutdown" in output
+
 
 def main(host, username, password):
     with paramiko.SSHClient() as cli:
@@ -17,30 +61,9 @@ def main(host, username, password):
 
         ssh(cli, 'gscli -c "system; netconf; nacm; disable"')
 
-        run("rm -rf id_rsa id_rsa.pub")
-        run("ssh-keygen -f id_rsa -N ''")
-        ssh(cli, "mkdir -p /home/admin/.ssh")
-        scp = SCPClient(cli.get_transport())
-        scp.put("id_rsa.pub", "/home/admin/.ssh/authorized_keys")
-        ssh(cli, "chown admin:admin /home/admin/.ssh/authorized_keys")
+        test_get(cli, host)
 
-        with open("/tmp/check_np2.sh", "w") as f:
-            f.write(
-                f"""#!/bin/sh
-
-netopeer2-cli <<EOF
-auth pref publickey 4
-auth keys add id_rsa.pub id_rsa
-auth hostkey-check disable
-connect --host {host} --login admin
-status
-get --filter-xpath "/goldstone-interfaces:*"
-get-config --source running --filter-xpath "/goldstone-tai:modules"
-
-EOF
-"""
-            )
-        run("chmod +x /tmp/check_np2.sh && /tmp/check_np2.sh")
+        test_interface_admin_set(cli, host)
 
 
 if __name__ == "__main__":
