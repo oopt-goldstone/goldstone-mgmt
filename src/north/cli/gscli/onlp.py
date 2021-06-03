oopt-goldstone/goldstone-mgmt

commit 518d60fb044b9bb5103b82ee724d56ebfbac28cd

north/cli: refactor ONLP information retrieval
Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

src/north/cli/gscli/onlp.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 stdout = logging.getLogger("stdout")
 stderr = logging.getLogger("stderr")
 
+
 def to_human(d):
     for key, val in d.items():
         if val == 0xFFFF:
@@ -45,14 +46,10 @@ class Component(object):
     def get_state_attr(self, details, component):
         table = []
         try:
-            data = self.component["components"]["component"][component][details][
-                "state"
-            ]
+            data = component[details]["state"]
             data = to_human(data)
             if details != "piu" and details != "sfp":
-                desc = self.component["components"]["component"][component]["state"][
-                    "description"
-                ]
+                desc = component["state"]["description"]
                 table.append(["description", desc])
             for k, v in data.items():
                 subnode = data[k]
@@ -77,7 +74,6 @@ class Component(object):
 
     def show_onlp(self, option="all"):
 
-        self.component = self.sr_op.get_data(self.XPATH, "operational")
         if option == "all":
             types = ["fan", "psu", "led", "piu", "sfp", "thermal", "sys"]
             for type_ in types:
@@ -120,8 +116,11 @@ class Component(object):
             stdout.info("Note: Values with the symbol '-' are unsupported")
 
     def get_components(self, type_):
-        c = self.component.get("components", {}).get("component", [])
-        return natsorted([v["name"] for v in c if v["state"]["type"] == type_.upper()])
+        c = self.sr_op.get_data(
+            f"{self.XPATH}/component[state/type='{type_.upper()}']", "operational"
+        )
+        c = c.get("components", {}).get("component", [])
+        return natsorted(c, key=lambda v: v["name"])
 
     def tech_support(self):
         stdout.info("\n Show Onlp details")

commit 142b688345af020020f71e9492e9a5eea57c3043

north/cli: fix printing the name of component (#29)
bug introduced by https://github.com/galileowistron/goldstone-mgmt/commit/518d60fb044b9bb5103b82ee724d56ebfbac28cd

Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

src/north/cli/gscli/onlp.py
@@ -85,7 +85,7 @@ class Component(object):
                 components = self.get_components(type_)
                 for component in components:
                     table = self.get_state_attr(type_, component)
-                    stdout.info(component)
+                    stdout.info(component['name'])
                     stdout.info(tabulate(table))
             stdout.info("Note: Values with the symbol '-' are unsupported")
 
@@ -93,25 +93,25 @@ class Component(object):
             components = self.get_components("piu")
             for component in components:
                 table = self.get_state_attr("piu", component)
-                stdout.info(component)
+                stdout.info(component['name'])
                 stdout.info(tabulate(table))
             components = self.get_components("sfp")
             for component in components:
                 table = self.get_state_attr("sfp", component)
-                stdout.info(component)
+                stdout.info(component['name'])
                 stdout.info(tabulate(table))
         elif option == "system":
             components = self.get_components("sys")
             for component in components:
                 table = self.get_state_attr("sys", component)
-                stdout.info(component)
+                stdout.info(component['name'])
                 stdout.info(tabulate(table))
 
         else:
             components = self.get_components(option)
             for component in components:
                 table = self.get_state_attr(option, component)
-                stdout.info(component)
+                stdout.info(component['name'])
                 stdout.info(tabulate(table))
             stdout.info("Note: Values with the symbol '-' are unsupported")
 