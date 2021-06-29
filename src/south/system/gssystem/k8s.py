a7f3a63fd35ccba559f92fc7c17ed0f697822415
Wataru Ishida <wataru.ishid@gmail.com>
2021-06-30 00:36:19 +0900

    south/system: always encure a route exists for k8s service CIDR (#44)
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- src/south/system/gssystem/k8s.py
+++ src/south/system/gssystem/k8s.py
@@ -4,9 +4,12 @@ import asyncio
 import os
 import logging
 import os.path
+import pyroute2
 
 KUBECONFIG = os.getenv("KUBECONFIG", "/etc/rancher/k3s/k3s.yaml")
 K8S_SERVICE = os.getenv("K8S_SERVICE", "k3s.service")
+SERVICE_CIDR = os.getenv("SERVICE_CIDR", "10.43.0.0/16")
+K8S_INTF_NAME = os.getenv("K8S_INTF_NAME", "cni0")
 
 logger = logging.getLogger(__name__)
 
@@ -60,6 +63,14 @@ class KubernetesServer:
                     if await self.ping():
                         break
                     await asyncio.sleep(5)
+
+            with pyroute2.NDB() as ndb:
+                try:
+                    intf = ndb.interfaces[K8S_INTF_NAME]
+                    ndb.routes.create(dst=SERVICE_CIDR,oif=intf["index"]).commit()
+                except Exception as error:
+                    logger.debug(f"{str(error)}")
+
 
     async def start(self):
 
