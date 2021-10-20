c4a6dba89b3eb17db96c308f8e31bfb910165c53
Wataru Ishida <wataru.ishid@gmail.com>
2021-10-20 09:30:28 +0000

    curve out the south core library
    introduced namespaced packages
    
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ src/lib/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="goldstone_lib",
+    version="0.1.0",
+    description="Goldstone Python Library",
+    url="https://github.com/microsonic/goldstone-mgmt",
+    python_requires=">=3.7",
+    packages=["goldstone.lib"],
+    zip_safe=False,
+)
