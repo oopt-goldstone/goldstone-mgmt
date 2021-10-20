e92a3301751974ff4d01db0d0ffe02021758ca4d
Wataru Ishida <wataru.ishid@gmail.com>
2021-10-20 10:39:50 +0000

    south/tai: change to namespace package layout
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- src/south/tai/setup.py
+++ src/south/tai/setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt", "r") as f:
     install_requires = f.read().split()
 
 setuptools.setup(
-    name="gstai",
+    name="goldstone_south_tai",
     version="0.1.0",
     install_requires=install_requires,
     description="Goldstone Python TAI south daemon",
@@ -12,9 +12,9 @@ setuptools.setup(
     python_requires=">=3.7",
     entry_points={
         "console_scripts": [
-            "gssouthd-tai = gstai.main:main",
+            "gssouthd-tai = goldstone.south.tai.main:main",
         ],
     },
-    packages=setuptools.find_packages(),
+    packages=["goldstone.south.tai"],
     zip_safe=False,
 )
