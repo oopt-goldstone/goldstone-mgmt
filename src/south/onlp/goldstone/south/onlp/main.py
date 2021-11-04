de72ae5eed35f292c70f03d91e9e227a79e1fa8d
Wataru Ishida <wataru.ishid@gmail.com>
2021-11-04 02:23:39 +0000

    south/onlp: use the core library
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

---
+++ src/south/onlp/goldstone/south/onlp/main.py
@@ -0,0 +1,62 @@
+import logging
+import asyncio
+import argparse
+import signal
+from .platform import PlatformServer
+import sysrepo
+from goldstone.lib.core import start_probe
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    async def _main():
+        loop = asyncio.get_event_loop()
+        stop_event = asyncio.Event()
+        loop.add_signal_handler(signal.SIGINT, stop_event.set)
+        loop.add_signal_handler(signal.SIGTERM, stop_event.set)
+
+        conn = sysrepo.SysrepoConnection()
+        server = PlatformServer(conn)
+
+        try:
+            tasks = await server.start()
+
+            runner = await start_probe("/healthz", "0.0.0.0", 8080)
+            tasks.append(stop_event.wait())
+            done, pending = await asyncio.wait(
+                tasks, return_when=asyncio.FIRST_COMPLETED
+            )
+            logger.debug(f"done: {done}, pending: {pending}")
+            for task in done:
+                e = task.exception()
+                if e:
+                    raise e
+        finally:
+            await runner.cleanup()
+            await server.stop()
+            conn.disconnect()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    args = parser.parse_args()
+
+    fmt = "%(levelname)s %(module)s %(funcName)s l.%(lineno)d | %(message)s"
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, format=fmt)
+        for noisy in [
+            "hpack",
+            "kubernetes.client.rest",
+            "kubernetes_asyncio.client.rest",
+        ]:
+            l = logging.getLogger(noisy)
+            l.setLevel(logging.INFO)
+    #        sysrepo.configure_logging(py_logging=True)
+    else:
+        logging.basicConfig(level=logging.INFO, format=fmt)
+
+    asyncio.run(_main())
+
+
+if __name__ == "__main__":
+    main()
