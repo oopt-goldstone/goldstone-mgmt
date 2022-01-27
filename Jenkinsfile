oopt-goldstone/goldstone-mgmt

commit cb73a7ec6a602966bf2de5af18151b81805a1060

north/snmp: use the connector library
also retrieve the interface info one by one to reduce load
of south-gearbox and south-sonic

Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

src/north/snmp/src/gs_ax_impl/mibs/ietf/rfc1213.py
@@ -1,5 +1,6 @@
-import sysrepo as sr
-import libyang
+from goldstone.lib.connector.sysrepo import Connector as SysrepoConnector
+from goldstone.lib.connector import Error
+
 from enum import unique, Enum
 from bisect import bisect_right
 from natsort import natsorted
@@ -16,7 +17,7 @@ from ax_interface.mib import (
 
 from ax_interface.encodings import ObjectIdentifier
 
-sysrepo_conn = sr.SysrepoConnection()
+sysrepo_conn = SysrepoConnector()
 
 
 @unique
@@ -54,14 +55,10 @@ class SystemUpdater(MIBUpdater):
         sysDescription = "Goldstone Version"
 
         xpath = "/goldstone-system:system/state/software-version"
-        with sysrepo_conn.start_session() as sess:
-            try:
-                sess.switch_datastore("operational")
-                data = sess.get_data(xpath)
-                version = data["system"]["state"]["software-version"]
-                # mibs.logger.warning(f"Goldstone version: {version}")
-            except Exception as e:
-                mibs.logger.warning(f"sysDesc Exception: {e}")
+        try:
+            version = sysrepo_conn.get(xpath)
+        except Error as e:
+            mibs.logger.warning(f"sysDesc Exception: {e}")
 
         return f"{sysDescription} {version}"
 
@@ -144,14 +141,14 @@ class InterfacesUpdater(MIBUpdater):
     def reinit_data(self):
         xpath = "/goldstone-interfaces:interfaces/interface"
         self.interfaces = []
-        with sysrepo_conn.start_session() as sess:
-            try:
-                sess.switch_datastore("operational")
-                data = sess.get_data(xpath)
-                ifs = list(libyang.xpath_get(data, xpath, [], filter=True))
-                self.interfaces = natsorted(ifs, key=lambda v: v["name"])
-            except Exception as e:
-                mibs.logger.warning(f"reinit_data Exception: {e}")
+        try:
+            names = sysrepo_conn.get_operational(xpath + "/name")
+            ifs = []
+            for name in names:
+                ifs.append(sysrepo_conn.get_operational(xpath + f"[name='{name}']"))
+            self.interfaces = natsorted(ifs, key=lambda v: v["name"])
+        except Error as e:
+            mibs.logger.warning(f"reinit_data Exception: {e}")
 
     def update_data(self):
         self.reinit_data()

commit 8170b1243f1f5f640751c62fbc9ca22d41f78fc9

north/snmp: fix regression
introduced by cb73a7ec6a602966bf2de5af18151b81805a1060

Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

Jenkinsfile
@@ -283,7 +283,7 @@ pipeline {
             stage('Test SNMP on amd64') {
               steps {
                 sh 'make tester'
-                sh "docker run -t -v `pwd`:`pwd` -w `pwd` gs-test/gs-mgmt-test:latest-amd64 python3 -m ci.tools.test_snmp ${params.DEVICE}"
+                sh "docker run -t -v `pwd`:`pwd` -w `pwd` gs-test/gs-mgmt-test:latest-amd64 python3 -m ci.tools.test_snmp ${params.DEVICE} --arch ${ARCH}"
               }
             }
           }
@@ -300,7 +300,7 @@ pipeline {
             stage('Test SNMP on arm64') {
               steps {
                 sh 'ARCH=amd64 make tester' // tester image doesn't need to be arm64
-                sh "docker run -t -v `pwd`:`pwd` -w `pwd` gs-test/gs-mgmt-test:latest-amd64 python3 -m ci.tools.test_snmp ${params.ARM_DEVICE}"
+                sh "docker run -t -v `pwd`:`pwd` -w `pwd` gs-test/gs-mgmt-test:latest-amd64 python3 -m ci.tools.test_snmp ${params.ARM_DEVICE} --arch ${ARCH}"
               }
             }
           }

src/north/snmp/src/gs_ax_impl/mibs/ietf/rfc1213.py
@@ -51,12 +51,11 @@ class SystemUpdater(MIBUpdater):
         return self.range[right]
 
     def system_desc(self):
-        version = "Init"
         sysDescription = "Goldstone Version"
 
         xpath = "/goldstone-system:system/state/software-version"
         try:
-            version = sysrepo_conn.get(xpath)
+            version = sysrepo_conn.get_operational(xpath, "unknown")
         except Error as e:
             mibs.logger.warning(f"sysDesc Exception: {e}")
 
@@ -145,7 +144,9 @@ class InterfacesUpdater(MIBUpdater):
             names = sysrepo_conn.get_operational(xpath + "/name")
             ifs = []
             for name in names:
-                ifs.append(sysrepo_conn.get_operational(xpath + f"[name='{name}']"))
+                ifs.append(
+                    sysrepo_conn.get_operational(xpath + f"[name='{name}']", one=True)
+                )
             self.interfaces = natsorted(ifs, key=lambda v: v["name"])
         except Error as e:
             mibs.logger.warning(f"reinit_data Exception: {e}")

ci/tools/test_snmp.py
@@ -6,19 +6,16 @@ import paramiko
 from .common import *
 
 
-def main(host, username, password):
+def main(host, username, password, arch):
 
     with paramiko.SSHClient() as cli:
         cli.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         cli.connect(host, username=username, password=password)
 
         run(f"snmpwalk -v 2c -c admin {host} system")
-
-        try:
-            run(f"snmpwalk -v 2c -c admin {host} ifTable")
-        except Exception as e:
-            print("TODO: snmpwalk ifTable can fail now after breakout configuration")
-            ssh(cli, "kubectl logs ds/gs-mgmt-snmp agentx")
+        version = run(f"snmpwalk -v 2c -c admin {host} SNMPv2-MIB::sysDescr.0")
+        assert "unknown" not in version
+        run(f"snmpwalk -v 2c -c admin {host} ifTable")
 
 
 if __name__ == "__main__":
@@ -26,6 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("host")
     parser.add_argument("--username", default="root")
     parser.add_argument("--password", default="x1")
+    parser.add_argument("--arch", default="amd64", choices=["amd64", "arm64"])
 
     args = parser.parse_args()
-    main(args.host, args.username, args.password)
+    main(args.host, args.username, args.password, args.arch)