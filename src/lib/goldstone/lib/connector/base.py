8f05485d8197018b55eaa16bef5170906e87453e
Wataru Ishida <wataru.ishid@gmail.com>
2022-01-27 04:39:46 +0000

    lib: add NETCONF connector
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- src/lib/goldstone/lib/connector/base.py
+++ src/lib/goldstone/lib/connector/base.py
@@ -1,4 +1,11 @@
+import sys
+
+
 class Error(Exception):
+    pass
+
+
+class NotSupported(Error):
     pass
 
 
@@ -17,4 +24,75 @@ class Session(object):
 
 
 class Connector(object):
-    pass
+    @property
+    def type(self):
+        raise "base"
+
+    def new_session(self, ds):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    @property
+    def models(self):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def find_node(self, xpath):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def save(self, model):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def rpc(self, xpath, args):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def set(self, xpath, value):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def delete(self, xpath):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def delete_all(self, model):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def apply(self):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def discard_changes(self):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def get(
+        self,
+        xpath,
+        default=None,
+        include_implicit_defaults=False,
+        strip=True,
+        one=False,
+        ds="running",
+    ):
+
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def get_operational(
+        self,
+        xpath,
+        default=None,
+        include_implicit_defaults=False,
+        strip=True,
+        one=False,
+    ):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
+
+    def get_startup(self, xpath):
+        fname = sys._getframe().f_code.co_name
+        raise NotSupported(f"{fname}() not supported by {self.type} connector")
---
+++ src/lib/goldstone/lib/connector/netconf.py
@@ -0,0 +1,266 @@
+from .base import (
+    Connector as BaseConnector,
+    Session as BaseSession,
+    Node,
+    Error,
+)
+
+import libyang
+import logging
+import urllib.parse
+from functools import cache
+
+from ncclient import manager
+from ncclient.xml_ import *
+from lxml import etree
+
+from pyang import repository
+from pyang import context
+from pyang.plugins.jsonxsl import JsonXslPlugin
+from pyang import syntax
+import io
+import json
+
+
+logger = logging.getLogger(__name__)
+
+
+def jsonxsl_xformer(models):
+
+    repos = repository.FileRepository()
+
+    ctx = context.Context(repos)
+
+    jsonxsl = JsonXslPlugin()
+    jsonxsl.setup_fmt(ctx)
+
+    modules = []
+
+    for filename, text in models:
+        m = syntax.re_filename.search(filename)
+        name, rev, in_format = m.groups()
+        module = ctx.add_module(
+            filename,
+            text,
+            in_format,
+            name,
+            rev,
+            expect_failure_error=False,
+            primary_module=True,
+        )
+        modules.append(module)
+
+    ctx.validate()
+
+    buf = io.StringIO()
+    jsonxsl.emit(ctx, modules, buf)
+    xslt = buf.getvalue()
+
+    # no idea why we need this, but without this, it failes to resolv URL
+    # there might be a better and cleaner way
+    class FileResolver(etree.Resolver):
+        def resolve(self, url, pubid, context):
+            return self.resolve_filename("/" + url, context)
+
+    parser = etree.XMLParser()
+    parser.resolvers.add(FileResolver())
+
+    return etree.XSLT(etree.parse(io.BytesIO(xslt.encode()), parser))
+
+
+class Session(BaseSession):
+    def __init__(self, conn, ds):
+        self.conn = conn
+        self.ds = ds
+
+    def delete_all(self, model):
+        if self.ds != "running":
+            super().delete_all(model)
+        return self.conn.delete_all(model)
+
+    def apply(self):
+        if self.ds != "running":
+            super().delete_all(model)
+        return self.conn.apply()
+
+
+class Connector(BaseConnector):
+    def __init__(self, **kwargs):
+        key = "hostkey_verify"
+        v = kwargs.get(key)
+        if v == "false":
+            kwargs[key] = False
+        elif v == "true":
+            kwargs[key] = True
+        self.conn = manager.connect(**kwargs)
+        assert self.conn != None
+        self._models = {}
+        xform_models = []
+        for cap in self.conn.server_capabilities:
+            t = urllib.parse.urlparse(cap)
+            q = [v.split("=") for v in t.query.split("&") if v]
+            q = {q[0]: q[1] for q in q}
+            m = q.get("module")
+            if not m:
+                continue
+
+            if t.scheme == "http":
+                ns = cap.split("?")[0]
+            elif t.scheme == "urn":
+                ns = f"urn:{t.path}"
+            else:
+                raise Error(f"unsupported scheme: {t.scheme}")
+            self._models[m] = {"query": q, "ns": ns}
+
+            # limit to Goldstone primitive models for speed
+            if "goldstone" in m:
+                schema = self.conn.get_schema(identifier=m)
+                xform_models.append((m + ".yang", schema._data))
+                self._models[m]["schema"] = schema._data
+
+        ctx = libyang.Context()
+        failed = []
+        for k, m in self._models.items():
+            schema = m.get("schema")
+            if schema:
+                try:
+                    mm = ctx.parse_module_str(schema)
+                except libyang.util.LibyangError as e:
+                    # depends on the order loading may fail due to dependency
+                    failed.append(m)
+
+        # try loading the failed module again
+        for f in failed:
+            mm = ctx.parse_module_str(f["schema"])
+
+        self.ctx = ctx
+
+        f = jsonxsl_xformer(xform_models)
+        self.xform = lambda v: json.loads(str(f(v)))
+
+    def new_session(self, ds="running"):
+        return Session(self, ds)
+
+    @property
+    def type(self):
+        return "netconf"
+
+    def xpath2xml(self, xpath, value=None):
+        xpath = list(libyang.xpath_split(xpath))
+        if len(xpath) == 0:
+            return None
+        node = xpath[0][1]
+        model = xpath[0][0]
+        v = self._models.get(model)
+        if v:
+            root = new_ele_ns(node, v["ns"])
+        else:
+            root = new_ele(node)
+
+        cur = root
+        for i, e in enumerate(xpath[1:]):
+            cur = sub_ele(cur, e[1])
+            for cond in e[2]:
+                ccur = cur
+                for cc in cond[0].split("/"):
+                    ccur = sub_ele(ccur, cc)
+                ccur.text = cond[1]
+
+        if value:
+            cur.text = value
+        else:
+            cur.set("{urn:ietf:params:xml:ns:netconf:base:1.0}operation", "delete")
+
+        logger.debug(
+            f"xpath: {xpath}, value: {value} xml: {etree.tostring(root).decode()}"
+        )
+
+        return root
+
+    def get_ns_from_xpath(self, xpath):
+        return {
+            elem[0]: self._models.get(elem[0])["ns"]
+            for elem in libyang.xpath_split(xpath)
+            if elem[0]  # elem[0] == prefix
+        }
+
+    @property
+    def models(self):
+        return self._models.keys()
+
+    def set(self, xpath, value):
+        v = self.xpath2xml(xpath, value)
+        config = new_ele("config")
+        config.append(v)
+        logger.debug(f"xml: {etree.tostring(config).decode()}")
+        return self.conn.edit_config(config)
+
+    def delete(self, xpath):
+        return self.set(xpath, None)
+
+    def delete_all(self, model):
+        m = self.ctx.get_module(model)
+        for n in m:
+            if n.keyword() in ["container", "leaf", "leaf-list", "list"]:
+                self.delete(f"/{model}:{n.name()}")
+
+    def apply(self):
+        return self.conn.commit()
+
+    def discard_changes(self):
+        self.conn.discard_changes()
+
+    def get(
+        self,
+        xpath,
+        default=None,
+        include_implicit_defaults=False,
+        strip=True,
+        one=False,
+        ds="running",
+    ):
+        nss = self.get_ns_from_xpath(xpath)
+        logger.debug(f"{xpath=}, {ds=}, {nss=}")
+
+        if ds == "operational":
+            v = self.conn.get(filter=("xpath", (nss, xpath)))
+        elif ds == "running":
+            v = self.conn.get_config(source=ds, filter=("xpath", (nss, xpath)))
+        else:
+            super().get(xpath, default, include_implicit_defaults, strip, one, ds)
+
+        logger.debug(f"data_xml: {v.data_xml}")
+
+        data = self.xform(etree.fromstring(v.data_xml.encode()))
+
+        assert len(data.keys()) < 2
+        if len(data.keys()) == 1:
+            key = list(data.keys())[0]
+            data[key.split(":")[-1]] = data[key]
+
+        if not strip:
+            return data
+
+        data = libyang.xpath_get(
+            data, xpath, default=default, filter=ds == "operational"
+        )
+        if data and one:
+            if len(data) == 1:
+                data = data[0]
+            elif len(data) > 1:
+                raise Error(f"{xpath} matches more than one item: {data}")
+
+        logger.debug(f"xpath: {xpath}, ds: {ds}, value: {data}")
+        return data
+
+    def get_operational(
+        self,
+        xpath,
+        default=None,
+        include_implicit_defaults=False,
+        strip=True,
+        one=False,
+    ):
+        return self.get(
+            xpath, default, include_implicit_defaults, strip, one, ds="operational"
+        )
