b5f003646259cbe55e38d9538250ea336b94ccca
Wataru Ishida <wataru.ishid@gmail.com>
2022-09-12 16:41:54 +0900

    south/netlink: support getting IPv4 address/neighbor info
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- src/south/netlink/src/server.rs
+++ src/south/netlink/src/server.rs
@@ -8,23 +8,69 @@ use sysrepo2::{Error, ErrorCode};
 
 use yang2::context::Context;
 use yang2::data::{Data, DataFormat, DataPrinterFlags, DataTree};
-use yang2::schema::SchemaNodeKind;
+use yang2::schema::{SchemaNode, SchemaNodeKind};
 
 use futures::channel::mpsc::UnboundedReceiver;
 use futures::stream::{StreamExt, TryStreamExt};
-use rtnetlink::packet::RtnlMessage;
+use rtnetlink::packet::{NeighbourMessage, RtnlMessage, AF_INET, NDA_DST};
 use rtnetlink::proto::NetlinkMessage;
 use rtnetlink::proto::NetlinkPayload::InnerMessage;
 use rtnetlink::{
     constants::RTMGRP_LINK,
     new_connection, packet,
+    packet::rtnl::address::nlas::Nla as AddrNla,
     packet::rtnl::link::nlas::{Nla, State},
+    packet::rtnl::neighbour::nlas::Nla as NeighNla,
     sys::{AsyncSocket, SocketAddr},
+    IpVersion,
 };
 
 use crate::xpath::xpath_split;
 
-fn get_info_from_nlas<'a>(nlas: &'a Vec<Nla>) -> SResult<(&'a str, Option<&'a str>, Option<u32>)> {
+fn get_addr_info_from_nlas(nlas: &Vec<AddrNla>) -> SResult<Option<String>> {
+    let iter = nlas.into_iter();
+
+    let mut addr = None;
+
+    for nla in iter {
+        match nla {
+            AddrNla::Address(a) => {
+                addr = Some(format!("{}.{}.{}.{}", a[0], a[1], a[2], a[3]));
+            }
+            _ => (),
+        }
+    }
+
+    Ok(addr)
+}
+
+fn get_neigh_info_from_nlas(nlas: &Vec<NeighNla>) -> SResult<(Option<String>, Option<String>)> {
+    let iter = nlas.into_iter();
+
+    let mut dst = None;
+    let mut lladdr = None;
+
+    for nla in iter {
+        match nla {
+            NeighNla::Destination(d) => {
+                dst = Some(format!("{}.{}.{}.{}", d[0], d[1], d[2], d[3]));
+            }
+            NeighNla::LinkLocalAddress(a) => {
+                lladdr = Some(format!(
+                    "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
+                    a[0], a[1], a[2], a[3], a[4], a[5]
+                ));
+            }
+            _ => (),
+        }
+    }
+
+    Ok((dst, lladdr))
+}
+
+fn get_link_info_from_nlas<'a>(
+    nlas: &'a Vec<Nla>,
+) -> SResult<(&'a str, Option<&'a str>, (Option<u32>, Option<String>))> {
     let mut iter = nlas.into_iter();
     let nla = iter.next();
 
@@ -36,6 +82,7 @@ fn get_info_from_nlas<'a>(nlas: &'a Vec<Nla>) -> SResult<(&'a str, Option<&'a st
 
     let mut state = None;
     let mut mtu = None;
+    let mut lladdr = None;
 
     for nla in iter {
         match nla {
@@ -54,11 +101,17 @@ fn get_info_from_nlas<'a>(nlas: &'a Vec<Nla>) -> SResult<(&'a str, Option<&'a st
                 };
                 mtu = Some(m);
             }
+            Nla::Address(a) => {
+                lladdr = Some(format!(
+                    "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
+                    a[0], a[1], a[2], a[3], a[4], a[5]
+                ));
+            }
             _ => (),
         }
     }
 
-    Ok((ifname, state, mtu))
+    Ok((ifname, state, (mtu, lladdr)))
 }
 
 fn get_module_top_container(conn: &Arc<Mutex<Connection>>, module_name: &str) -> SResult<String> {
@@ -81,6 +134,7 @@ fn get_module_top_container(conn: &Arc<Mutex<Connection>>, module_name: &str) ->
 struct InnerServer {
     module_name: String,
     module_top_container: String,
+    ipv4_enabled: bool,
     handle: rtnetlink::Handle,
     ctx: Arc<Context>,
 }
@@ -97,9 +151,16 @@ impl InnerServer {
 
         let module_top_container = get_module_top_container(conn, &module_name)?;
 
+        let ipv4 = format!("{}/interface/goldstone-ipv4:ipv4", module_top_container);
+        let ipv4_enabled = match ctx.find_xpath(ipv4.as_str()) {
+            Ok(v) => v.collect::<Vec<SchemaNode>>().len() != 0,
+            Err(_) => false,
+        };
+
         Ok(InnerServer {
             module_name,
             module_top_container,
+            ipv4_enabled,
             handle,
             ctx,
         })
@@ -181,32 +242,105 @@ impl InnerServer {
     }
 
     async fn oper_get_items_callback(&self, _xpath: &str) -> SResult<DataTree> {
+        let neighbours: Vec<NeighbourMessage> = self
+            .handle
+            .neighbours()
+            .get()
+            .set_family(IpVersion::V4)
+            .execute()
+            .try_collect()
+            .await
+            .map_err(|e| {
+                println!("error: {:?}", e);
+                Error::new(ErrorCode::Internal)
+            })?;
+
         let mut links = self.handle.link().get().execute();
         let mut data = DataTree::new(&self.ctx);
         while let Some(msg) = links.try_next().await.unwrap() {
-            let (ifname, state, mtu) = get_info_from_nlas(&msg.nlas)?;
+            let idx = msg.header.index;
+            let (ifname, state, (mtu, lladdr)) = get_link_info_from_nlas(&msg.nlas)?;
+            let prefix = format!("{}/interface[name='{}']", self.module_top_container, ifname);
 
-            let prefix = format!(
-                "/{}:{}/interface[name='{}']",
-                self.module_name, self.module_top_container, ifname
-            );
             let xpath = format!("{}/name", prefix);
             data.new_path(&xpath, Some(ifname), false)?;
             let xpath = format!("{}/state/oper-status", prefix);
             data.new_path(&xpath, state, false)?;
-            if let Some(mtu) = mtu {
-                let xpath = format!("{}/ethernet/state/mtu", prefix);
-                let mtu = mtu.to_string();
-                data.new_path(&xpath, Some(mtu.as_str()), false)?;
-            }
-
-            let xpath = format!("{prefix}/state/admin-status");
+            let xpath = format!("{}/state/admin-status", prefix);
             let admin_status = if msg.header.flags & packet::IFF_UP > 0 {
                 "UP"
             } else {
                 "DOWN"
             };
             data.new_path(&xpath, Some(&admin_status), false)?;
+
+            if let Some(mtu) = mtu {
+                let xpath = format!("{}/ethernet/state/mtu", prefix);
+                let mtu = mtu.to_string();
+                data.new_path(&xpath, Some(mtu.as_str()), false)?;
+            }
+
+            if let Some(lladdr) = lladdr {
+                let xpath = format!("{}/ethernet/state/mac-address", prefix);
+                data.new_path(&xpath, Some(lladdr.as_str()), false)?;
+            }
+
+            if !self.ipv4_enabled {
+                continue;
+            }
+
+            let mut addrs = self
+                .handle
+                .address()
+                .get()
+                .set_link_index_filter(idx)
+                .execute();
+            while let Some(msg) = addrs.try_next().await.unwrap() {
+                if msg.header.index == idx && msg.header.family == AF_INET as u8 {
+                    let addr = get_addr_info_from_nlas(&msg.nlas)?;
+                    if addr.is_none() {
+                        log::warn!("invalid address message: {:?}", msg);
+                        continue;
+                    }
+                    let addr = addr.unwrap();
+                    let addr_prefix = format!(
+                        "{}/goldstone-ipv4:ipv4/addresses/address[ip='{addr}']",
+                        prefix
+                    );
+                    let xpath = format!("{}/config/ip", addr_prefix);
+                    data.new_path(&xpath, Some(&addr), false)?;
+
+                    let xpath = format!("{}/state/ip", addr_prefix);
+                    data.new_path(&xpath, Some(&addr), false)?;
+                    let xpath = format!("{}/state/prefix-length", addr_prefix);
+                    let prefix_len = msg.header.prefix_len.to_string();
+                    data.new_path(&xpath, Some(prefix_len.as_str()), false)?;
+                }
+            }
+
+            for n in &neighbours {
+                if n.header.ifindex == idx && n.header.ntype == NDA_DST as u8 {
+                    let (dst, lladdr) = get_neigh_info_from_nlas(&n.nlas)?;
+                    if dst.is_none() {
+                        log::warn!("invalid neighbor message: {:?}", n);
+                        continue;
+                    }
+                    let dst = dst.unwrap();
+                    let neigh_prefix = format!(
+                        "{}/goldstone-ipv4:ipv4/neighbors/neighbor[ip='{}']",
+                        prefix, dst
+                    );
+                    let xpath = format!("{}/config/ip", neigh_prefix);
+                    data.new_path(&xpath, Some(&dst), false)?;
+
+                    let xpath = format!("{}/state/ip", neigh_prefix);
+                    data.new_path(&xpath, Some(&dst), false)?;
+                    if let Some(lladdr) = lladdr {
+                        let xpath = format!("{}/state/link-layer-address", neigh_prefix);
+                        data.new_path(&xpath, Some(&lladdr), false)?;
+                    }
+                }
+            }
         }
         Ok(data)
     }
@@ -259,7 +393,7 @@ impl Server {
         let prefix = format!("/{}:interface-link-state-notify-event", &module_name);
         while let Some((message, _)) = messages.next().await {
             if let InnerMessage(packet::rtnl::RtnlMessage::NewLink(m)) = message.payload {
-                let (ifname, state, _) = get_info_from_nlas(&m.nlas)?;
+                let (ifname, state, _) = get_link_info_from_nlas(&m.nlas)?;
                 let mut data = DataTree::new(&ctx);
 
                 let xpath = format!("{}/if-name", prefix);
@@ -305,5 +439,29 @@ impl Server {
         )?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    use crate::server::Server;
+    use sysrepo2::connection::Connection;
+    use sysrepo2::session::Session;
+    use sysrepo2::types::{ConnectionOptions, DatastoreType};
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn test_subscribe_module_change_async() {
+        let conn =
+            Connection::new(ConnectionOptions::DEFAULT).expect("Failed to create connection");
+        let conn = Arc::new(Mutex::new(conn));
+        let _server = Server::new(&conn, "goldstone-interfaces").expect("Failed to create server");
+
+        let sess =
+            Session::new(&conn, DatastoreType::OPERATIONAL).expect("Failed to create session");
+
+        sess.get_item("/goldstone-interfaces:interfaces", 0)
+            .expect("Failed to get interface info");
     }
 }
